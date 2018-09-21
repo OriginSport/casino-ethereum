aab455ec25b17239d7b5d49342a0659d279f8c5c
XatMassacrE <xxat.massacre@gmail.com>
2018-09-21 11:01:23 +0800

    Add basic authority contract

--- contracts/Casino.sol
+++ contracts/Casino.sol
@@ -1,8 +1,16 @@
 pragma solidity ^0.4.24;
 
 import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
+import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
+import './Signable.sol';
 
-contract Casino {
+contract Casino is Ownable, Signable {
+
+  event LogParticipant(address indexed player);
+
+  constructor() public {
+    owner = msg.sender;
+  }
 
 }
  
