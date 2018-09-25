fe54efd650fb05f5bb81b0715e32e6f29b66e5af
root <admin@geeknet.com>
2018-09-25 17:14:11 +0800

    add properties.js

---
+++ test/properties.js
@@ -0,0 +1,32 @@
+module.exports = {
+    development: {
+        networkID: '5777',
+        contractAbi: [],
+        contractAddress: '',
+        url: 'http://127.0.0.1:7545',
+        from: '',
+        pk: '',
+        gasPrice: 10000000000,
+        gasLimit: 4100000
+    },
+    ropsten: {
+        networkID: '3',
+        contractAbi: [],
+        contractAddress: '',
+        url: 'https://ropsten.infura.io/',
+        from: '',
+        pk: '',
+        gasPrice: 1100000000,
+        gasLimit: 4100000
+    },
+    mainnet: {
+        networkID: '1',
+        contractAbi: [],
+        contractAddress: '',
+        url: 'https://mainnet.infura.io/',
+        from: '',
+        pk: '',
+        gasPrice: 4000000000,
+        gasLimit: 7800000
+    }
+}
