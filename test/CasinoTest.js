4992a838edff599d2aef17cb1d01c805ad8755ee
bingozwb <bingo_zwb@163.com>
2018-09-29 20:14:37 +0800

    Add recharge and withdraw test

--- test/CasinoTest.js
+++ test/CasinoTest.js
@@ -1,13 +1,15 @@
-const {web3, property, sendSignedTx, sendSignedTxSimple, evmMine, getString} = require('./contractUtil.js')
+const {web3, property, sendSignedTx, sendSignedTxHelper, sendSignedTxSimple, evmMine, getString} = require('./contractUtil.js')
+const {getEventLogs, getEvents, decodeLog} = require('./eventUtil')
 
 const Account = require("eth-lib/lib/account");
 
 const contractJson = require('../build/contracts/Casino.json')
 const contractAbi = contractJson.abi
 const contractAddr = contractJson.networks[property.networkID].address
+const transactionHash = contractJson.networks[property.networkID].transactionHash
 const contract = new web3.eth.Contract(contractAbi, contractAddr)
 
-const reveals = ['0','1','2','3','4','5','6','7','8','9']
+const reveals = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 function getCommit(reveal) {
   return web3.utils.soliditySha3(reveal)
@@ -18,7 +20,7 @@ async function placeBet(value, choice, modulo, expiredBlockNumber, commit, v, r,
   const nonce = await web3.eth.getTransactionCount(property.from)
   const gasPrice = await web3.eth.getGasPrice()
   const gasLimit = await web3.eth.estimateGas({from: property.from, to: contractAddr, value: value, data: data})
-  await sendSignedTx(contractAddr, data, nonce, value, gasPrice, gasLimit, property.from, property.pk)
+  return await sendSignedTx(contractAddr, data, nonce, value, gasPrice, gasLimit, property.from, property.pk)
 }
 
 async function closeBet(reveal) {
@@ -31,6 +33,17 @@ async function refundBet(commit) {
   return await sendSignedTxSimple(contractAddr, data)
 }
 
+async function withdraw(amount) {
+  const data = contract.methods.withdraw(amount).encodeABI()
+  return await sendSignedTxSimple(contractAddr, data)
+}
+
+async function recharge(amount) {
+  const data = contract.methods.recharge().encodeABI()
+  return await sendSignedTxHelper(contractAddr, data, amount, property.pk)
+}
+
+
 function getSignature(expiredBlockNumber, commit, pk) {
   const msgHash = web3.utils.soliditySha3(expiredBlockNumber, commit)
   const signature = Account.sign(msgHash, pk)
@@ -41,6 +54,10 @@ function getSignature(expiredBlockNumber, commit, pk) {
 }
 
 async function test() {
+  // console.log(contractJson.abi)
+  const events = getEvents(contractJson.abi)
+  const fromBlock = (await web3.eth.getTransaction(transactionHash)).blockNumber
+  console.log('fromBlock', fromBlock)
 
   const contBan = await web3.eth.getBalance(contractAddr)
   console.log('contract balance', contBan)
@@ -53,43 +70,50 @@ async function test() {
   const choice = '1'
   const modulo = '2'
   const expiredBlockNumber = await web3.eth.getBlockNumber() + 200
-
+  // const expiredBlockNumber = 5000000
   const commit = getCommit(reveal)
+
   const sign = getSignature(expiredBlockNumber, commit, property.pk)
+  // console.log(commit, ', ', web3.utils.hexToNumber(sign[1]), ', ', sign[2], ', ', sign[3])
+  // console.log('reveal', reveal)
 
-  console.log(sign[0])
-  console.log(web3.utils.hexToNumber(sign[1]))
+  await placeBet(value, choice, modulo, expiredBlockNumber, commit, web3.utils.hexToNumber(sign[1]), sign[2], sign[3])
+    .then(tx => {
+      console.log(decodeLog(events.LogParticipant, tx.logs[0]))
+    })
 
-  // await placeBet(value, choice, modulo, expiredBlockNumber, commit, web3.utils.hexToNumber(sign[1]), sign[2], sign[3])
+  await closeBet(reveal).then(tx => {
+    const logs = tx.logs
+    console.log(decodeLog(events.LogClosedBet, logs[logs.length - 1]))
+  })
 
-  // await closeBet(reveal).then(tx => {
-  //   console.log(tx.logs)
+  // await refundBet(commit).then(tx => {
+  //   console.log(decodeLog(events.LogRefund, tx.logs[0]))
   // })
 
-  // await refundBet(2).then(tx => {
-  //   console.log(tx.logs)
+  // await recharge(web3.utils.toWei('10', 'ether')).then(tx => {
+  //   console.log(decodeLog(events.LogRecharge, tx.logs[0]))
+  // })
+
+  // await withdraw(web3.utils.toWei('0.5', 'ether')).then(tx => {
+  //   console.log(decodeLog(events.LogDealerWithdraw, tx.logs[0]))
   // })
 
   // for (let i = 0; i < 250; i++) {
   //   await evmMine()
   // }
 
-  // await web3.eth.getTransactionReceipt('0x3dc2a18b67e3ae93c9e996ace6910ec92950720559337d303e1d6a7c544fefc5')
-  //   .then(tx => {
-  //     console.log(tx.logs[1].topics[1].slice())
-  //     console.log(web3.utils.hexToNumberString(tx.logs[1].data.slice(2, 66)))
-  //     console.log(web3.utils.hexToNumberString(tx.logs[1].data.slice(66, 130)))
-  //     console.log(web3.utils.hexToNumberString(tx.logs[1].data.slice(130, 194)))
-  //     console.log(web3.utils.hexToNumberString(tx.logs[1].data.slice(194, 258)))
+  // const eventAbi = events.LogDealerWithdraw
+  const eventAbi = events.LogRecharge
+  // console.log(eventAbi)
+  // const encodeEventSignature = web3.eth.abi.encodeEventSignature(eventAbi)
+  // await getEventLogs(fromBlock, null, contractAddr, [eventAbi.signature])
+  //   .then(logs => {
+  //     for (const o of logs) {
+  //       const dl = decodeLog(eventAbi, o)
+  //       console.log(dl)
+  //     }
   //   })
-  // const blockNumber = await web3.eth.getBlockNumber()
-  // console.log('blockNumber', blockNumber)
-  // for (let i = 0; i < blockNumber; i++) {
-  //   const block = await web3.eth.getBlock(i)
-  //   console.log(block.miner, block.timestamp)
-  // }
-
-
 }
 
 test()
