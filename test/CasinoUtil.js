d9c960766f8ef616752e5e6eef327dd024aabc74
bingozwb <bingo_zwb@163.com>
2018-10-08 17:07:48 +0800

    Add getOdds function

--- test/CasinoUtil.js
+++ test/CasinoUtil.js
@@ -8,8 +8,39 @@ function closeBet(reveal, blockHash, module) {
   return{random: random.toString(16), result: result.toString()}
 }
 
-const reveal = '0x' + '000000000000000000000000000000000000000000000000000ddee4def596f7'
-const commit = web3.utils.sha3(reveal)
-const blockHash = '0xac62d92f3c1eec9b2574db3a06c6b3cba28628bc4bb47b640e72515eaa9d65d3'
-console.log(commit)
-console.log(closeBet(reveal, blockHash, 2))
+
+function odds(_amount, _choice, _modulo) {
+  const HOUSE_EDGE_PERCENT = 1
+  const HOUSE_EDGE_MINIMUM_AMOUNT = 300000000000000
+
+  // population count
+  const POPCOUNT_MULT = new BigNumber(0x0000000000002000000000100000000008000000000400000000020000000001)
+  const POPCOUNT_MASK = new BigNumber(0x0001041041041041041041041041041041041041041041041041041041041041)
+  const POPCOUNT_MODULO = 0x3F
+
+  console.log(POPCOUNT_MULT, POPCOUNT_MASK, POPCOUNT_MODULO)
+
+  let houseEdge = new BigNumber(_amount).multipliedBy(HOUSE_EDGE_PERCENT).dividedBy(100)
+  if (houseEdge < HOUSE_EDGE_MINIMUM_AMOUNT) {
+    houseEdge = HOUSE_EDGE_MINIMUM_AMOUNT;
+    console.log('if true')
+  }
+  console.log('houseEdge', houseEdge)
+
+  let populationCount
+  if (_modulo < 40) {
+    const m = POPCOUNT_MULT.multipliedBy(_choice)
+    const mm = new BigNumber(POPCOUNT_MULT.multipliedBy(_choice) & POPCOUNT_MASK)
+    populationCount = new BigNumber(POPCOUNT_MULT.multipliedBy(_choice) & POPCOUNT_MASK).modulo(POPCOUNT_MODULO)
+    console.log(m, mm, populationCount)
+  } else {
+    populationCount = _choice
+  }
+
+  const winAmount = (_amount - houseEdge) * _modulo / populationCount
+  const odds = winAmount / _amount
+  const winChance = populationCount / _modulo
+  return {winAmount, odds, winChance}
+}
+
+console.log(odds(1000000000000000000, 1, 2))
