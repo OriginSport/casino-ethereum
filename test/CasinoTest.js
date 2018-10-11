785367afd86fac7557e44fd445668c8892bf8b6c
bingozwb <bingo_zwb@163.com>
2018-10-11 17:24:25 +0800

    Add setSigner, setCroupier test. Add method comments. Update methods

--- test/CasinoTest.js
+++ test/CasinoTest.js
@@ -5,57 +5,142 @@ const Account = require("eth-lib/lib/account");
 
 const contractJson = require('../build/contracts/Casino.json')
 const contractAbi = contractJson.abi
-const contractAddr = contractJson.networks[property.networkID].address
-const transactionHash = contractJson.networks[property.networkID].transactionHash
+const networkID = property.networkID
+const contractAddr = contractJson.networks[networkID].address
 const contract = new web3.eth.Contract(contractAbi, contractAddr)
 
-const reveals = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-function getCommit(reveal) {
-  return web3.utils.soliditySha3(reveal)
+/**
+ * get commit by sha3(_reveal)
+ * @param _reveal
+ * @returns {*}
+ */
+function getCommit(_reveal) {
+  return web3.utils.soliditySha3(_reveal)
 }
 
-async function placeBet(value, choice, modulo, expiredBlockNumber, commit, v, r, s) {
-  const data = contract.methods.placeBet(choice, modulo, expiredBlockNumber, commit, v, r, s).encodeABI()
-  const nonce = await web3.eth.getTransactionCount(property.from)
-  const gasPrice = await web3.eth.getGasPrice()
-  const gasLimit = await web3.eth.estimateGas({from: property.from, to: contractAddr, value: value, data: data})
-  return await sendSignedTx(contractAddr, data, nonce, value, gasPrice, gasLimit, property.from, property.pk)
+/**
+ * get bet information
+ * @param _commit bet key provide by croupier
+ * @returns {Promise<*>}
+ */
+async function getBet(_commit) {
+  return await contract.methods.bets(_commit).call()
 }
 
-async function closeBet(reveal) {
-  const data = contract.methods.closeBet(reveal).encodeABI()
-  return await sendSignedTxSimple(contractAddr, data)
+/**
+ * place bet
+ * @param _value bet amount
+ * @param _choice  bet mask choice(binary convert to decimal)
+ * @param _modulo  bet model. eg. coin:2, dice:6, dice*2:36, rollUnder:100
+ * @param _expiredBlockNumber  expiration blockNumber limit
+ * @param _commit  bet key provide by croupier
+ * @param _v signature provide by croupier
+ * @param _r signature provide by croupier
+ * @param _s signature provide by croupier
+ * @param _pk private key of player account
+ * @returns {Promise<*>}
+ */
+async function placeBet(_value, _choice, _modulo, _expiredBlockNumber, _commit, _v, _r, _s, _pk) {
+  const data = contract.methods.placeBet(_choice, _modulo, _expiredBlockNumber, _commit, _v, _r, _s).encodeABI()
+  return await sendSignedTxHelper(contractAddr, data, _value, _pk)
 }
 
-async function refundBet(commit) {
-  const data = contract.methods.refundBet(commit).encodeABI()
-  return await sendSignedTxSimple(contractAddr, data)
+/**
+ * close bet which is active
+ * @param _reveal bet secret key provide by croupier
+ * @param _pk private key of croupier account
+ * @returns {Promise<*>}
+ */
+async function closeBet(_reveal, _pk) {
+  const data = contract.methods.closeBet(_reveal).encodeABI()
+  return await sendSignedTxHelper(contractAddr, data, null, _pk)
 }
 
-async function withdraw(amount) {
-  const data = contract.methods.withdraw(amount).encodeABI()
-  return await sendSignedTxSimple(contractAddr, data)
+/**
+ * refund bet expired
+ * @param _commit bet key provide by croupier
+ * @param _pk private key of croupier account
+ * @returns {Promise<*>}
+ */
+async function refundBet(_commit, _pk) {
+  const data = contract.methods.refundBet(_commit).encodeABI()
+  return await sendSignedTxHelper(contractAddr, data, null, _pk)
 }
 
-async function recharge(amount) {
+/**
+ * withdraw available balance in casino contract
+ * @param _amount lt (casino.balance - bankFund)
+ * @param _pk private key of owner account
+ * @returns {Promise<*>}
+ */
+async function withdraw(_amount, _pk) {
+  const data = contract.methods.withdraw(_amount).encodeABI()
+  return await sendSignedTxHelper(contractAddr, data, null, _pk)
+}
+
+/**
+ * recharge ether to casino contract
+ * @param _amount recharge amount
+ * @param _pk private key of from account
+ * @returns {Promise<*>}
+ */
+async function recharge(_amount, _pk) {
   const data = contract.methods.recharge().encodeABI()
-  return await sendSignedTxHelper(contractAddr, data, amount, property.pk)
+  return await sendSignedTxHelper(contractAddr, data, _amount, _pk)
 }
 
+/**
+ * transfer signer, only owner and signer can invoke
+ * @param _address  new signer address
+ * @param _pk private key of signer account
+ * @returns {Promise<*>}
+ */
+async function setSigner(_address, _pk) {
+  const data = contract.methods.transferSigner(_address).encodeABI()
+  return await sendSignedTxHelper(contractAddr, data, null, _pk)
+}
 
-function getSignature(expiredBlockNumber, commit, pk) {
-  const msgHash = web3.utils.soliditySha3(expiredBlockNumber, commit)
-  const signature = Account.sign(msgHash, pk)
+/**
+ * transfer croupier, only owner and croupier can invoke
+ * @param _address  new croupier address
+ * @param _pk private key of croupier account
+ * @returns {Promise<*>}
+ */
+async function setCroupier(_address, _pk) {
+  const data = contract.methods.transferCroupier(_address).encodeABI()
+  return await sendSignedTxHelper(contractAddr, data, null, _pk)
+}
+
+/**
+ * get signature of message hash sha3(expiredBlockNumber, commit)
+ * @param _expiredBlockNumber  expiration blockNumber limit
+ * @param _commit  bet key provide by croupier
+ * @param _pk  private key of signer account
+ * @returns {*[]}
+ */
+function getSignature(_expiredBlockNumber, _commit, _pk) {
+  const msgHash = web3.utils.soliditySha3(_expiredBlockNumber, _commit)
+  const signature = Account.sign(msgHash, _pk)
   const r = signature.slice(0, 66)
   const s = '0x' + signature.slice(66, 130)
   const v = signature.slice(130, 132)
   return [signature, v, r, s]
 }
 
+
 async function test() {
-  // console.log(contractJson.abi)
-  const events = getEvents(contractJson.abi)
+  const reveals = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  let reveal = reveals[6]
+  let commit = getCommit(reveal)
+
+  const playerPk = property.pk
+  const signerPk = property.pk
+  const croupierPk = property.pk
+  const ownerPk = property.pk
+
+  const events = getEvents(contractAbi)
+  const transactionHash = contractJson.networks[networkID].transactionHash
   const fromBlock = (await web3.eth.getTransaction(transactionHash)).blockNumber
   console.log('fromBlock', fromBlock)
 
@@ -64,47 +149,53 @@ async function test() {
   const accBan = await web3.eth.getBalance(property.from)
   console.log('acc1 balance', accBan)
 
-  const reveal = reveals[0]
-
   const value = 1e17
   const choice = '1'
   const modulo = '2'
   const expiredBlockNumber = await web3.eth.getBlockNumber() + 200
-  // const expiredBlockNumber = 5000000
-  const commit = getCommit(reveal)
 
-  const sign = getSignature(expiredBlockNumber, commit, property.pk)
-  // console.log(commit, ', ', web3.utils.hexToNumber(sign[1]), ', ', sign[2], ', ', sign[3])
-  // console.log('reveal', reveal)
+  // const sign = getSignature(expiredBlockNumber, commit, property.pk)
+  // await placeBet(value, choice, modulo, expiredBlockNumber, commit, web3.utils.hexToNumber(sign[1]), sign[2], sign[3], playerPk)
+  //   .then(tx => {
+  //     console.log(decodeLog(events.LogParticipant, tx.logs[0]))
+  //   })
 
-  await placeBet(value, choice, modulo, expiredBlockNumber, commit, web3.utils.hexToNumber(sign[1]), sign[2], sign[3])
-    .then(tx => {
-      console.log(decodeLog(events.LogParticipant, tx.logs[0]))
-    })
+  // await closeBet(reveal, croupierPk).then(tx => {
+  //   const logs = tx.logs
+  //   console.log(decodeLog(events.LogClosedBet, logs[logs.length - 1]))
+  // })
 
-  await closeBet(reveal).then(tx => {
-    const logs = tx.logs
-    console.log(decodeLog(events.LogClosedBet, logs[logs.length - 1]))
-  })
-
-  // await refundBet(commit).then(tx => {
+  // await refundBet(commit, croupierPk).then(tx => {
   //   console.log(decodeLog(events.LogRefund, tx.logs[0]))
   // })
 
-  // await recharge(web3.utils.toWei('10', 'ether')).then(tx => {
+  // await recharge(web3.utils.toWei('1', 'ether'), croupierPk).then(tx => {
   //   console.log(decodeLog(events.LogRecharge, tx.logs[0]))
   // })
 
-  // await withdraw(web3.utils.toWei('0.5', 'ether')).then(tx => {
+  // await withdraw(web3.utils.toWei('0.5', 'ether'), ownerPk).then(tx => {
   //   console.log(decodeLog(events.LogDealerWithdraw, tx.logs[0]))
   // })
 
-  // for (let i = 0; i < 250; i++) {
+  // await getBet(getCommit(reveal)).then(data => console.log(data))
+
+  // contract.methods.signer().call().then(data => console.log('signer', data))
+  // await setSigner('0x243d416DEF4fA6eF0842837d5F72D51d7AF36791', ownerPk).then(tx => {
+  //   console.log(decodeLog(events.SignerTransferred, tx.logs[0]))
+  // })
+
+  // contract.methods.croupier().call().then(data => console.log('croupier', data))
+  // await setCroupier('0x243d416DEF4fA6eF0842837d5F72D51d7AF36791', ownerPk).then(tx => {
+  //   console.log(decodeLog(events.CroupierTransferred, tx.logs[0]))
+  // })
+
+
+  // for (let i = 0; i < 251; i++) {
   //   await evmMine()
   // }
 
-  // const eventAbi = events.LogDealerWithdraw
-  const eventAbi = events.LogRecharge
+  // let eventAbi = events.LogDealerWithdraw
+  // eventAbi = events.LogRecharge
   // console.log(eventAbi)
   // const encodeEventSignature = web3.eth.abi.encodeEventSignature(eventAbi)
   // await getEventLogs(fromBlock, null, contractAddr, [eventAbi.signature])
@@ -117,3 +208,4 @@ async function test() {
 }
 
 test()
+
